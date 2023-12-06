f4610b13f0508c91d64d855557b3a6350a0cd3d8
obf1313 <oubaifan@163.com>
2023-12-06 21:15:34 +0800

    feat: 补全注释、新增标记、开启无限存储权限

--- background.js
+++ background.js
@@ -1,17 +1,23 @@
+/** 对比之前数据和本次数据变更 */
 const diffJSON = (before, now) => {
   if (before.updatedAt === now.updatedAt) {
     console.log('无变更')
   } else {
     const diffArr = []
+    const deleteArr = []
+    const newArr = []
     // 判断删除和变更
-    diffAndMark(before.components, now.components, diffArr)
-    // TODO: 新增是不是要反着找？
+    diff(before.components, now.components, diffArr, deleteArr)
+    // 判断新增
+    diff(before.components, now.components, [], newArr)
+    /** 因为 background.js 只能做服务端操作，不能进行 dom 操作，所以需要通过 postMessage 形式通知 content */
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, diffArr)
+      chrome.tabs.sendMessage(tabs[0].id, { diffArr, deleteArr, newArr })
     })
   }
 }
 
+/** 查找匹配元素 */
 const findNowElement = (nowArr, id) => {
   for (let i = 0; i < nowArr.length; i++) {
     const nowElement = nowArr[i]
@@ -23,35 +29,41 @@ const findNowElement = (nowArr, id) => {
   }
 }
 
-const diffAndMark = (beforeArr, nowArr, diffArr) => {
+/** 递归判断节点 */
+const diff = (beforeArr, nowArr, diffArr, deleteArr) => {
   for (let i = 0; i < beforeArr.length; i++) {
     const beforeElement = beforeArr[i]
     if (Array.isArray(beforeElement.components)) {
-      diffAndMark(nowElement.components, nowArr, diffArr)
+      diff(nowElement.components, nowArr, diffArr, deleteArr)
     } else {
       const nowElement = findNowElement(nowArr, beforeElement._id)
       if (nowElement) {
-        // 对比两个元素是否有差异，如果有则高亮该元素
         if (JSON.stringify(beforeElement) !== JSON.stringify(nowElement)) {
           diffArr.push(beforeElement._id)
         }
       } else {
-        console.log('有元素被删除了')
+        deleteArr.push(beforeElement._id)
       }
     }
   }
 }
 
 let getOverList = []
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.action.setBadgeText({
     text: 'ON',
   })
 })
+
+// 监听请求
 chrome.webRequest.onResponseStarted.addListener(
   details => {
+    // TODO: 此处切换 Tab 就不会执行了，考虑用定时器的方式替换、或者其他更好的方法？
+    // 因为会进入循环
     if (!getOverList.includes(details.url)) {
       getOverList.push(details.url)
+      // 自己请求一次
       fetch(details.url, {
         method: details.method,
       })
@@ -65,13 +77,16 @@ chrome.webRequest.onResponseStarted.addListener(
                   Object.keys(result).length === 0) ||
                 !result
               ) {
+                // 设置浏览器存储该数据
                 chrome.storage.local.set({
                   [nodeID]: JSON.stringify(data.payload[0]),
                 })
               } else {
+                // 如果之前存过该页面的数据
                 const before = JSON.parse(result[nodeID])
                 const now = data.payload[0]
                 diffJSON(before, now)
+                // TODO: 对比后是用当前这次的替换掉初次的，还是手动清除？
               }
             })
           }
--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,15 @@
-const markElement = id => {
+/** 标记元素 */
+const markElement = (id, type = 'update') => {
+  const setting = {
+    update: {
+      backgroundColor: 'rgba(0, 255, 0, 0.5)',
+      border: '1px solid rgb(0, 255, 0)',
+    },
+    new: {
+      backgroundColor: 'rgba(255, 0, 0, 0.5)',
+      border: '1px solid rgb(255, 0, 0)',
+    },
+  }
   const el = document.getElementById(id)
   if (el) {
     const newEl = document.createElement('div')
@@ -7,8 +18,8 @@ const markElement = id => {
     newEl.style.left = 0
     newEl.style.width = '100%'
     newEl.style.height = '100%'
-    newEl.style.backgroundColor = 'rgba(0, 255, 255, 0.5)'
-    newEl.style.border = '1px solid rgb(255, 255, 0)'
+    newEl.style.backgroundColor = setting[type].backgroundColor
+    newEl.style.border = setting[type].border
     newEl.onclick = () => {
       newEl.remove()
     }
@@ -16,8 +27,17 @@ const markElement = id => {
   }
 }
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (Array.isArray(request)) {
-    request.forEach(item => markElement(item))
+/** 之前被删除的元素怎么处理呢 */
+const showDeleteElement = () => {}
+
+chrome.runtime.onMessage.addListener(function ({ diffArr, deleteArr, newArr }) {
+  if (Array.isArray(diffArr)) {
+    diffArr.forEach(item => markElement(item))
+  }
+  if (Array.isArray(deleteArr)) {
+    deleteArr.forEach(item => showDeleteElement(item))
+  }
+  if (Array.isArray(newArr)) {
+    newArr.forEach(item => markElement(item))
   }
 })
